9bcf260ef38d3a0e70acd4be6de26c3daa51aaf1
perilu <15778895371@163.com>
2022-09-21 23:21:04 +0800

    feat: add a api to get msg

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('欢迎使用微信云托管！')
 })
-
+app.get('/msg', (req, res) => {
+  res.send('hello world 欢迎使用微信云托管！')
+})
 const port = process.env.PORT || 80
 app.listen(port, () => {
   console.log('服务启动成功，端口：', port)
